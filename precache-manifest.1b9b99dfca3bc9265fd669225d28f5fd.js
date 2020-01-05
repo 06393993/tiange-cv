b8e1606491244aa3738a0e5caaf7f0540687ce98
kaiyi <06393993lky@gmail.com>
2020-01-05 15:07:30 -0800

    Updates

---
+++ precache-manifest.1b9b99dfca3bc9265fd669225d28f5fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e7e998cf77bda1b67a772e49150c85",
+    "url": "/tiange-cv/index.html"
+  },
+  {
+    "revision": "f8278651cca3ca67fa77",
+    "url": "/tiange-cv/static/css/main.a4763039.chunk.css"
+  },
+  {
+    "revision": "17ef72cbccd6e6c0407f",
+    "url": "/tiange-cv/static/js/2.6a7bb2f9.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/tiange-cv/static/js/2.6a7bb2f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8278651cca3ca67fa77",
+    "url": "/tiange-cv/static/js/main.2a8bf962.chunk.js"
+  },
+  {
+    "revision": "449bbffb133f569dee82",
+    "url": "/tiange-cv/static/js/runtime-main.bdd2db7b.js"
+  },
+  {
+    "revision": "35e13c652cb457a085d05b1ca9852f47",
+    "url": "/tiange-cv/static/media/learningtogroup_v1.35e13c65.png"
+  },
+  {
+    "revision": "1e0eeb71771a6f20d84aee6dab35a642",
+    "url": "/tiange-cv/static/media/portrait.1e0eeb71.jpg"
+  }
+]);
